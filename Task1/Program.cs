d47838a76f6812a86ea4f4d8077353cefde841ee
Andr3wBro <andr3wbro@gmail.com>
2022-06-09 17:20:57 +0300

    home work done

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("Введите первое число : ");
+string number1 = Console.ReadLine();
+Console.Write("Введите второе число : ");
+string number2 = Console.ReadLine();
+int numberInt1 = int.Parse(number1);
+int numberInt2 = int.Parse(number2);
+int max=0, min=0;
+    if (numberInt1>numberInt2)
+    {
+        max=numberInt1;
+        min=numberInt2;
+        Console.WriteLine($"Первое число больше и равно : {max}\nВторое число меньше и равно {min}");
+    }
+    else
+    {
+        max=numberInt2;
+        min=numberInt1;
+        Console.WriteLine($"Второе число больше и равно : {max}\nПервое число меньше и равно : {min}");
+    }
---
+++ Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Введите первое число :");
+string number1 = Console.ReadLine();
+Console.Write("Введите второе число :");
+string number2 = Console.ReadLine();
+Console.Write("Введите третье число :");
+string number3 = Console.ReadLine();
+int numberInt1 = int.Parse(number1);
+int numberInt2 = int.Parse(number2);
+int numberInt3 = int.Parse(number3);
+int max=0;
+    if (max<numberInt1)
+    {
+        max=numberInt1;
+    }
+    if (max<numberInt2)
+    {
+        max=numberInt2;
+    }
+    if (max<numberInt3)
+    {
+        max=numberInt3;
+    }
+Console.WriteLine($"Самое больше число : {max}");
